912ab98317ba4a9f3793b0a7a909ff78e7c0eea4
Bosang6 <huangzhikang66@gmail.com>
2026-05-03 15:56:10 +0200

    Test: Array allocation/deallocation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 //#include "AllocatorBenchmark.h"
 #include "MemoryFunctions.h"
+#include <iostream>
 
 class A 
 {
 public:
+    A()
+        :m_a(10), m_b(20)
+    { }
     A(int a, int b) {
         m_a = a;
         m_b = b;
+    }
+    ~A()
+    {
+        std::cout << "m_a = " << m_a << ", m_b = " << m_b << std::endl;
     }
 
 private:
@@ -20,6 +28,9 @@ int main()
     
     A* a = MM_NEW(A, 1, 2);
     MM_DELETE(a);
+
+    A* array = MM_NEW_A(A, 10);
+    MM_DELETE_A(array, 10);
 
     return 0;
 }
