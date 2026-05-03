d43df34daa5be3e6784e66b15e6656da60bc9c10
Bosang6 <huangzhikang66@gmail.com>
2026-05-03 23:51:55 +0200

    Add unsized global delete/delete[] overloads (Some compiler call unsized version)

--- include/MemoryManagerGlobal.h
+++ include/MemoryManagerGlobal.h
@@ -3,12 +3,21 @@
 
 #include "MemoryManager.h"
 
+#if defined(__cpp_sized_deallocation) || defined(_MSC_VER)
+#define MM_USE_SIZED_DELETE
+#endif
+
 #ifdef USE_MEMORY_MANAGER_GLOBAL
 
 void* operator new(std::size_t size);
-void operator delete(void* ptr, std::size_t size) noexcept;
-
 void* operator new[](std::size_t size);
+
+#if defined(MM_USE_SIZED_DELETE)
+void operator delete(void* ptr, std::size_t size) noexcept;
 void operator delete[](void* ptr, std::size_t size) noexcept;
+#endif
+
+void operator delete(void* ptr) noexcept;
+void operator delete[](void* ptr) noexcept;
 
 #endif
--- src/MemoryManagerGlobal.cpp
+++ src/MemoryManagerGlobal.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #ifdef USE_MEMORY_MANAGER_GLOBAL
 
+#pragma region Sized Version
+
+#ifdef MM_USE_SIZED_DELETE
 void* operator new(std::size_t size)
 {
     return MemoryManager::Instance().Allocate(size);
@@ -13,6 +16,7 @@ void operator delete(void* ptr, std::size_t size) noexcept
     MemoryManager::Instance().Deallocate(ptr, size);
 }
 
+
 void* operator new[](std::size_t size)
 {
     return MemoryManager::Instance().Allocate(size);
@@ -22,5 +26,70 @@ void operator delete[](void* ptr, std::size_t size) noexcept
 {
     MemoryManager::Instance().Deallocate(ptr, size);
 }
+#pragma endregion
+
+
+#pragma region Unsized Version
+#else
+struct AllocationHeader
+{
+    std::size_t totalSize;
+};
+
+void* operator new(std::size_t size)
+{
+    std::size_t totalSize = sizeof(AllocationHeader) + size;
+
+    void* totalPlace = MemoryManager::Instance().Allocate(totalSize);
+
+    AllocationHeader* header = static_cast<AllocationHeader*>(totalPlace);
+    header->totalSize = totalSize;
+
+    void* userPlace = header + 1;
+
+    return userPlace;
+}
+
+void operator delete(void* ptr) noexcept
+{
+    if(!ptr)
+    {
+        return;
+    }
+
+    AllocationHeader* header = static_cast<AllocationHeader*>(ptr) - 1;
+    MemoryManager::Instance().Deallocate(header, header->totalSize);
+}
+
+
+void* operator new[](std::size_t size)
+{
+    std::size_t totalSize = sizeof(AllocationHeader) + size;
+
+    void* totalPlace = MemoryManager::Instance().Allocate(totalSize);
+
+    AllocationHeader* header = static_cast<AllocationHeader*>(totalPlace);
+    header->totalSize = totalSize;
+
+    void* userPlace = header + 1;
+
+    return userPlace;
+}
+
+void operator delete[](void* ptr) noexcept
+{
+    if (!ptr)
+    {
+        return;
+    }
+
+    AllocationHeader* header = static_cast<AllocationHeader*>(ptr) - 1;
+    MemoryManager::Instance().Deallocate(header, header->totalSize);
+}
+#endif
+
+#pragma endregion
+
+
 
 #endif
