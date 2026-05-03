1427d5115db50da87fb16015259b6c154dcf70b7
Bosang6 <huangzhikang66@gmail.com>
2026-05-03 15:35:27 +0200

    Test: using macro to allocate/deallocate an object

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include "AllocatorBenchmark.h"
+//#include "AllocatorBenchmark.h"
+#include "MemoryFunctions.h"
+
+class A 
+{
+public:
+    A(int a, int b) {
+        m_a = a;
+        m_b = b;
+    }
+
+private:
+    int m_a, m_b;
+};
+
 
 int main()
 {
-    RunAllBenchmarks();
+    //RunAllBenchmarks();
     
+    A* a = MM_NEW(A, 1, 2);
+    MM_DELETE(a);
+
     return 0;
 }
