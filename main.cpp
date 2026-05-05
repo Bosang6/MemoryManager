683afc2f954caf5da1be8023300527cfa0f6d8b0
Bosang6 <huangzhikang66@gmail.com>
2026-05-05 22:39:01 +0200

    Cleanup

--- main.cpp
+++ main.cpp
@@ -1,36 +1,12 @@
-//#include "AllocatorBenchmark.h"
-#include "MemoryFunctions.h"
 #include <iostream>
 #include "MemoryManagerGlobal.h"
+#include "MemoryFunctions.h"
 #include "MMAllocator.h"
 #include "Bulk.h"
 #include "SmallObj_8byte.h"
 #include "SmallObj_32byte.h"
 int main()
 {
-    //RunAllBenchmarks();
-
-#pragma region Functions Test
-    //void* p = MM_MALLOC(10);
-    //MM_FREE(p, 10);
-    //
-    //A* a = MM_NEW(A, 1, 2);
-    //MM_DELETE(a);
-
-    //A* array = MM_NEW_A(A, 10);
-    //MM_DELETE_A(array, 10);
-#pragma endregion
-
-#pragma region Global Override Test
-    //A* a = new A();
-    //delete(a);
-
-    //A* arrayGeneral = new A[10]; // 10 * 8(sizeof(A)) + 8(array cookie) = 88 > 64 => GeneralAllocator
-    //delete[] arrayGeneral;
-
-    //A* array = new A[5]; // (5 * 8(sizeof(A)) + 8(array cookie) = 48 < 64 => SmallObjAllocator
-    //delete[] array;
-#pragma endregion
 
 #pragma region Custom Allocator in STL
     //std::vector<A, MMAllocator<A>> vec;
--- tests/SameOrder.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <cstddef>
-#include <vector>
-#include "Timer.h"
-#include "MemoryFunctions.h"
